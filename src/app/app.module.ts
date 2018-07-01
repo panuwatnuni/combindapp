bdbde15783458e64a143db4678cf6e1eafcec290
Panuwat Prada-in <panuwut.pradain@gmail.com>
2018-07-01 16:53:13 +0700

    combind menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,56 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { TabArticlePage } from '../pages/tab-article/tab-article';
+import { TabCoursePage } from '../pages/tab-course/tab-course';
+import { TabsPage } from '../pages/tabs/tabs';
+import { TabHomePage } from '../pages/tab-home/tab-home';
+import { TabServicePage } from '../pages/tab-service/tab-service';
+import { TabContactPage } from '../pages/tab-contact/tab-contact';
+import { SidePaymentPage } from '../pages/side-payment/side-payment';
+import { SidePortfolioPage } from '../pages/side-portfolio/side-portfolio';
+import { SideSchedulePage } from '../pages/side-schedule/side-schedule';
+import { SideSettingPage } from '../pages/side-setting/side-setting';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        SidePaymentPage,
+        SidePortfolioPage,
+        SideSchedulePage,
+        SideSettingPage,
+        TabArticlePage,
+        TabCoursePage,
+        TabHomePage,
+        TabServicePage,
+        TabContactPage,
+        TabsPage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp),
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        SidePaymentPage,
+        SidePortfolioPage,
+        SideSchedulePage,
+        SideSettingPage,
+        TabArticlePage,
+        TabCoursePage,
+        TabHomePage,
+        TabServicePage,
+        TabContactPage,
+        TabsPage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: ErrorHandler, useClass: IonicErrorHandler }
+    ]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TabHomePage } from '../tab-home/tab-home';
+import { TabCoursePage } from '../tab-course/tab-course';
+import { TabArticlePage } from './../tab-article/tab-article';
+import { TabServicePage } from '../tab-service/tab-service';
+import { TabContactPage } from '../tab-contact/tab-contact';
+
+@IonicPage()
+@Component({
+    selector: 'page-tabs',
+    templateUrl: 'tabs.html',
+})
+export class TabsPage {
+    //กำหนดตัวแปล tabs ต่าง
+    tab1Root:any = TabHomePage
+    tab2Root:any = TabCoursePage
+    tab3Root: any = TabServicePage
+    tab4Root: any = TabArticlePage
+    tab5Root:any = TabContactPage
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad TabsPage');
+    }
+
+}
